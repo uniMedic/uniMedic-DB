f5f6d3b384d1934556545270378f34cf9cf66b4a
HiroForYou <csanchezs@uni.pe>
2021-06-20 18:05:56 -0500

    colecciones actualizadas

--- models/Diagnosis.js
+++ models/Diagnosis.js
@@ -3,20 +3,20 @@ const { Schema, model } = require('mongoose');
 // Para guardar una imagen en mongoDB
 // https://stackoverflow.com/questions/29780733/store-an-image-in-mongodb-using-node-js-express-and-mongoose
 
-const diagnosisSchema = new Schema({
-	DiagnosticoID: {
+const diagnosticSchema = new Schema({
+	diagnosticID: {
 		type: String,
 		required: true
 	},
-	textDiagnosis: {
+	textDiagnostic: {
 		type: String
 	},
-	imageDiagnosis: {
+	imageDiagnostic: {
 		data: Buffer,
 		contentType: String
 	},
-	dateOfDiagnosis: {
+	dateOfDiagnostic: {
 		type: Date
 	}
 });
-module.exports = model('Diagnosis', diagnosisSchema);
+module.exports = model('Diagnostic', diagnosticSchema);
